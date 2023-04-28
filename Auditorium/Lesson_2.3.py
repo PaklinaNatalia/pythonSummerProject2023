a69e46116216314d0dd1b277350927e9a884a26a
Natalia Paklina <angelwomega@gmail.com>
2023-04-28 21:15:46 +0300

    Initial commit

--- Auditorium/Lesson_2.3.py
+++ Auditorium/Lesson_2.3.py
@@ -3,8 +3,18 @@ b = a[:]
 a[0] = 111
 print(*a)
 print(*b)
-
+print()
 a = [1, 2, 3, 4]
 print(*a)
 b = list('cat')
 print(*b)
+print()
+mass = [1, 2, 3, 4]
+print(len(mass))
+print(len('cat'))
+print()
+a = [10, 20, 30, 40]
+for k, v in enumerate(a):
+    print(k, v)
+print()
+print(*list(range(10)))
--- Auditorium/Lesson_2.5.py
+++
@@ -1,8 +0,0 @@
-input_list = [10, 'э', 15, 'ABC', 1]
-for i in input_list:
-    if type(i) == str:
-        print(i * 5)
-    elif type(i) == int:
-        print(i ** 2)
-    else:
-        print(i)
