9fb87da272da079d0c89992440e0ea5c53925482
Natalia Paklina <angelwomega@gmail.com>
2023-04-28 19:41:41 +0300

    Initial commit

---
+++ Auditorium/Lesson 2.0.py
@@ -0,0 +1,7 @@
+for i in range(10):
+    print(i, i ** 2, i ** 3)
+for i in range(10):
+    if i % 2 == 0:
+        print(f"{i} – четное")
+    else:
+        print (f"{i} – нечетное")
---
+++ Auditorium/Lesson 2.1.py
@@ -0,0 +1,10 @@
+n = int(input("Введите n: "))
+for n in range(1, n + 1):
+    if n % 15 == 0:
+        print("FizzBuzz")
+    elif n % 3 == 0:
+        print("Fizz")
+    elif n % 5 == 0:
+        print("Buzz")
+    else:
+        print(n)
