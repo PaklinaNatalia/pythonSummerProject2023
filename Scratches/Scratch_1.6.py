f845402451604df244b8a2ff354f04961ff3c3d7
Natalia Paklina <angelwomega@gmail.com>
2023-05-15 02:30:13 +0300

    Initial commit

---
+++ Scratches/Scratch_1.6.py
@@ -0,0 +1,15 @@
+
+def convert_to_roman(n):
+    m = ["", "M", "MM", "MMM"]
+    c = ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"]
+    x = ["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"]
+    i = ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"]
+    thousands = m[n // 1000]
+    hundreds = c[(n % 1000) // 100]
+    tens = x[(n % 100) // 10]
+    ones = i[n % 10]
+    roman = (thousands + hundreds + tens + ones)
+    return roman
+
+n = int(input("Введите число: "))
+print(f"Римский эквивалент: {convert_to_roman(n)}")
