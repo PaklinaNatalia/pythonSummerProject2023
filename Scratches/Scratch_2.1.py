4945aeda8738a5b5f441efc619ea714f9afd170e
Natalia Paklina <angelwomega@gmail.com>
2023-05-17 21:39:08 +0300

    Initial commit

--- Scratches/Scratch_2.1.py
+++ Scratches/Scratch_2.1.py
@@ -14,3 +14,30 @@ print()
 lst = [222, 21, 1, 111, 12, 322]
 print(sorted(lst, key = lambda x: (x % 10, x)))
 print()
+
+lst = [111, 22, 12, 21, 33, 5, 6, 66, 3]
+print(sorted(lst, key = lambda x: (x % 2, x)))
+print(sorted(lst, key = lambda x: (x % 2, x % 10, x)))
+print()
+
+lst = [111, 22, 12, 21, 33, 5, 6, 66, 3]
+print(sorted(lst, key = lambda x: (x % 2, -x)))
+print(sorted(lst, key = lambda x: (x % 2, x % 10, -x)))
+print()
+
+lst = input("Введите строку: ").split()
+print(sorted(lst, key = lambda x: x.lower()))
+print(sorted(lst, key = lambda x: x.upper()))
+print()
+
+numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+print(list(filter(lambda x: x % 2 == 0, numbers)))
+print(list(filter(lambda x: x % 2, numbers)))
+print(list(filter(lambda x: x % 3 == 0, numbers)))
+print(list(map(lambda x: x % 2 == 0, numbers)))
+print(list(map(lambda x: x % 2, numbers)))
+print(list(map(lambda x: x % 3 == 0, numbers)))
+print()
+
+numbers = [123, 234, 345, 456, 567, 678, 789, 890, 901]
+print(list(map(lambda x: sum(map(int, str(x))), numbers)))
