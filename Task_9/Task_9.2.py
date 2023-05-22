c435386e6f4ee844cabe01e8c99d7c0ec60a4412
Natalia Paklina <angelwomega@gmail.com>
2023-05-22 17:45:48 +0300

    Initial commit

--- Task_9/Task_9.2.py
+++ Task_9/Task_9.2.py
@@ -2,7 +2,7 @@ import collections
 f = open ("../Texts/test_05.txt", "r", encoding ="utf-8")
 s0 = f.read()
 s0 = s0.lower()
-s0 = sorted(dict(collections.Counter(s0)).items(), reverse=True)
+s0 = sorted(dict(collections.Counter(s0)).items(), key = lambda x: (-x[1], x[0]))
 for i, j in s0[:10]:
     print(i, "–", j)
 f.close()
