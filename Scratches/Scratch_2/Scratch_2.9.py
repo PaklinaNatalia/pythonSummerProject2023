af8ddfb8abdecefaf95cf84ed3fcd11da706d710
Natalia Paklina <angelwomega@gmail.com>
2023-05-29 19:02:56 +0300

    Initial commit

---
+++ Scratches/Scratch_2/Scratch_2.9.py
@@ -0,0 +1,16 @@
+print(*[[i for i in range(4)] for j in range(4)])
+print()
+print(*[[i + j for i in range(4)] for j in range(4)])
+print()
+print(*[[i * j for i in range(4)] for j in range(4)])
+print()
+print(*[[(lambda x, y: x ** y)(i, j) for i in range(4)] for j in range(4)])
+print()
+print(*[[[(lambda x, y, z: x + y + z)(i, j, k) for i in range(4)] for j in range(3)] for k in range(2)])
+print()
+matrix = [[1,2,3], [4,5,6], [7,8,9]], [[11,12,13], [14,15,16], [17,18,19]], [[21,22,23], [24,25,26], [27,28,29]], [[31,32,33], [34,35,36], [37,38,39]]
+flat = [k for i in matrix for j in i for k in j]
+print(*flat)
+print()
+print(*[[x * y for x in range(1, 6)] for y in range(1, 6)])
+print()
