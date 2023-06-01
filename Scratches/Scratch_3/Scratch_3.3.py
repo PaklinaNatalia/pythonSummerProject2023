92a7e8fa95c731acd9f5d48db71bb748119fcf62
Natalia Paklina <angelwomega@gmail.com>
2023-06-01 10:13:49 +0300

    Initial commit

---
+++ Scratches/Scratch_3/Scratch_3.3.py
@@ -0,0 +1,24 @@
+try:
+    raise Exception("Что-то не так...")
+except Exception as e:
+    print("Спасите! " + str(e))
+
+def f(n):
+    for x in range(n):
+        yield x ** 2
+
+g = f(10)
+while True:
+    try:
+        print(next(g))
+    except StopIteration:
+        print("Числа закончились!")
+        break
+
+def sumn(n):
+    if n == 0:
+        return 0
+    else:
+        return n + sumn(n - 1)
+
+print(sumn(int(input("Введите n: "))))
