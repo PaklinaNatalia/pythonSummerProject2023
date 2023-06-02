c9eca2f7ff1b710722022cb5d546e971f0b5beda
Natalia Paklina <angelwomega@gmail.com>
2023-06-02 16:29:48 +0300

    Initial commit

--- Task_14/Task_14.2.py
+++ Task_14/Task_14.2.py
@@ -1,5 +1,7 @@
 def sum_digits(n):
-    if n == 0:
+    if n < 0:
+        return ("Введенное n не является натуральным числом!")
+    elif n == 0:
         return 0
     else:
         return n%10 + sum_digits(n//10)
